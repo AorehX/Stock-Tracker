40f4fee7e7aa7421b8c4709aa95f9be66e47d1ca
AorehX <k-12@keemail.me>
2025-11-29 12:33:25 +0100

    The foundation

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <img src="" alt="logo" />
+      </div>
+
+      <nav>
+        <div>
+          <ul>
+            <li><a href="#">Services</a></li>
+            <li><a href="/">Stock Market</a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Our Develement Team</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <aside>
+        <div>
+
+        </div>
+      </aside>
+
+      <main>
+        <div>
+
+        </div>
+      </main>
+
+      <article>
+        <div>
+
+        </div>
+      </article>
+
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
